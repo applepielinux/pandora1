b487f523adb734f76adcb67208b0098a76d15155
applepielinux <130183599+applepielinux@users.noreply.github.com>
2024-04-05 16:01:08 -0400

    Add files via upload

---
+++ AnimationStrings.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class AnimationStrings
+{
+   internal static string isMoving = "isMoving";
+   internal static string isGrounded = "isGrounded";
+}
