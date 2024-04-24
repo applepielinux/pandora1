9e6565cba4870ad687c7eaba06fa37cf743cac68
applepielinux <130183599+applepielinux@users.noreply.github.com>
2024-04-24 14:54:06 -0400

    Update AnimationStrings.cs

--- AnimationStrings.cs
+++ AnimationStrings.cs
@@ -1,12 +1,20 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class AnimationStrings
+namespace Assets.Scripts
 {
-   internal static string isMoving = "isMoving";
-   internal static string isGrounded = "isGrounded";
+    internal class AnimationStrings
+    {
+        internal static string isMoving = "isMoving";
+        internal static string isGrounded = "isGrounded";
+        internal static string jump = "jump";
+        internal static string yVelocity = "yVelocity";
+        internal static string isAlive = "isAlive";
+        internal static string isOnWall = "isOnWall";
+        internal static string isOnCeiling = "isOnCeiling";
+    }
+
 }
